6fe5bed593e2f889e60666bc2f4e53f289b935a3
SommieOgu <Cynthiaogu04@gmail.com>
2026-01-17 17:22:49 -0500

    fix auth state listener to update UI correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/firebaseConfig";
+import { signUp, login, logout } from "./firebase/auth";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* Holds the currently logged-in Firebase user
+  null = not logged in */
+  const [user, setUser] = useState(null);
+
+  //Used to prevent UI changes during state checks
+  const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+    // Firebase Listener once page load, login and logout
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("User is logged in:", user.email);
+        setUser(user);
+      } else {
+        console.log("No user logged in");
+        setUser(null);
+      }
+
+       setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+ // Prevent rendering until Firebase finishes auth check
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  
   console.log("Firebase auth instance:", auth);
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+         {user ? (
+      <>
+        <p>Logged in as {user.email}</p>
+        <button onClick={logout}>Logout</button>
+      </>
+    ) : (
+      <>
+        <p>Not logged in</p>
+        <button onClick={() => signUp("test@email.com", "password123")}>
+          Sign Up
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => login("test@email.com", "password123")}
+        >
+          Login
+        </button>
+      </>
+    )} 
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div>
+      {user ? (
+        <p>Logged in as {user.email}</p>
+      ) : (
+        <p>Not logged in</p>
+      )}
+    </div>
     </>
-  )
+  );
 }
 
 export default App
